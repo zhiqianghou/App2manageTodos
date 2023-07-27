ecc230958f9402cc3e208c5dc0f1c28118cab2f7
zhiqianghou <zhiqiang.hou.ibp@gmail.com>
2023-07-27 06:59:01 -0500

    Handle missing file error

--- gui.py
+++ gui.py
@@ -1,7 +1,12 @@
 import functions
 import PySimpleGUI as sg
 import time
+import os
 
+
+if not os.path.exists("todos.txt"):
+	with open("todos.txt", "w") as file:
+		pass
 
 sg.theme("LightBrown1")
 
