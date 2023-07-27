b50ad266a0de2c8040569fa6216712553c7c50ec
zhiqianghou <zhiqiang.hou.ibp@gmail.com>
2023-07-27 05:42:19 -0500

    Implement the Add button with image icon

--- gui.py
+++ gui.py
@@ -8,9 +8,10 @@ sg.theme("LightBrown1")
 clock = sg.Text("",key="clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button(key="Add",image_source="add_image.png", mouseover_colors="LightBlue2",
+					   tooltip="Add todo")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
-					  enable_events=True, size=[45, 10])
+					  enable_events=True, size=(45,10))
 edit_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
