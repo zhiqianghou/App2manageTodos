a228d69356fe823e8d9bd2658771a7fa3a9febe3
zhiqianghou <zhiqiang.hou.ibp@gmail.com>
2023-07-30 13:11:28 -0500

    Implement "Complete new todo items" feature

--- web.py
+++ web.py
@@ -9,16 +9,21 @@ def add_todo():
 	todos.append(todo)
 	functions.write_todos(todos)
 
+
+
 st.title("My Todo App")
 st.subheader("This is my todo app")
 st.write("This app increase your productivity")
 
-for todo in todos:
-	st.checkbox(todo)
+for index, todo in enumerate(todos):
+	checkbox = st.checkbox(todo, key=todo)
+	if checkbox:
+		todos.pop(index)
+		functions.write_todos(todos)
+		del st.session_state[todo]
+		st.experimental_rerun()
 
 st.text_input(label="Add new items", placeholder="Enter new todo:",
 			  on_change=add_todo, key="new_todo")
-
-st.session_state
 
 
